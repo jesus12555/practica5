b3c37399cf9df100c463b9025fd41b9c2384cbaf
User_8H <a.20308051280625@cbtis128.edu.mx>
2023-03-27 18:03:45 +0000

    p5JESUS_LUGO_TAB bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,38 +3,55 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Pestaña tab',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        primarySwatch: Colors.lightBlue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(50), // Creates border
+                color: Colors.cyan), //Change background color from here
+            tabs: [
+              Tab(icon: Icon(Icons.accessibility_new)),
+              Tab(icon: Icon(Icons.account_circle_sharp)),
+              Tab(icon: Icon(Icons.add_home_work)),
+              Tab(icon: Icon(Icons.add_comment)),
+            ],
+          ),
+          title: Text('Tabs Demo'),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.accessibility_new, size: 350),
+            Icon(Icons.account_circle_sharp, size: 350),
+            Icon(Icons.add_home_work, size: 350),
+            Icon(Icons.add_comment, size: 350),
+          ],
         ),
       ),
     );
